55df2e21e8f553262b9585046f5016bb0100e267
Viajero <viajero95@mail.ru>
2026-07-24 19:35:34 +0700

    Введение в Pytest

---
+++ tests/test_pytest.py
@@ -0,0 +1,9 @@
+def test_first_try():
+    print('test_first_try')
+
+def test_assert_positive_test():
+    assert (2 + 2) == 4
+
+def test_assert_negative_test():
+    x, y = 5, 2
+    assert (y + y) == x
